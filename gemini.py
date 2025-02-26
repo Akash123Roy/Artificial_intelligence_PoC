250e9eb0153c7a1540f1534986d2ebb42f4e32c0
Akash Roy <111109999+Akash123Roy@users.noreply.github.com>
2025-02-26 14:42:10 +0530

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-genai.configure(api_key = "AIzaSyBjKe4Wk6CUtT0oSG1pUaq4Sn0ER90JpGY")
+genai.configure(api_key = "")
 model = genai.GenerativeModel(model_name= "gemini-1.5-flash")
 response = model.generate_content(["I want to know the meaning of life"])
 print(response)
